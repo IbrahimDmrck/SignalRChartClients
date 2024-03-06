be7f67224609ec940e254c7069011c5154c9103a
ibrahimdmrck@gmail.com <ibrahimdmrck@gmail.com>
2024-03-06 21:34:24 +0300

    SignalR Chart Project Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,79 @@
 import { Component } from '@angular/core';
 import * as Highcharts from "highcharts";
 import * as signalR from "@microsoft/signalr";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  connection: signalR.HubConnection;
   Highcharts: typeof Highcharts = Highcharts;
+  chart;
+  updateFromInput = false;
+  chartCallback;
 
-  chartOptions: Highcharts.Options =
-    {
-      //Grafik Title
+  constructor() {
+    this.connection = new signalR.HubConnectionBuilder().withUrl("https://localhost:7088/satishub").build();
+    this.connection.start();
+
+    this.connection.on("receiveMessage", message => {
+      console.log(message);
+      this.chart.showLoading();
+      for (let i = 0; i < this.chart.series.length; i++) {
+        this.chart.series[i].remove();
+
+      }
+
+      for (let i = 0; i < message.length; i++) {
+        this.chart.addSeries(message[i]);
+
+      }
+
+      this.updateFromInput = true;
+      this.chart.hideLoading();
+    });
+
+
+
+
+    const self = this;
+    this.chartCallback = chart => {
+      self.chart = chart;
+    };
+  }
+
+  chartOptions: Highcharts.Options = {
+    // Grafik Title
+    title: {
+      text: "Başlık"
+    },
+    subtitle: {
+      text: "Alt Başlık"
+    },
+    yAxis: {
       title: {
-        text: "Başlık"
-      },
-      subtitle: {
-        text: "Alt Başlık"
-      },
-      yAxis:{
-        title:{
-          text:"Y Ekseni"
-        }
-      },
-      xAxis:{
-        accessibility:{
-          rangeDescription:"2019 - 2024"
-        }
-      },
-      legend:{
-        layout:"vertical",
-        align:'right',
-        verticalAlign:"middle"
-      },
-      series:[
-       {
-        name:"X",
-        type: "line",
-        data:[1000,2000,3000]
-       },
-       {
-        name:"Y",
-        type: "line",
-        data:[4000,2755,7000]
-       },
-       {
-        name:"Z",
-        type: "line",
-        data:[6071,1453,1915]
-       }
-      ],
-      plotOptions:{
-        series:{
-          label:{
-            connectorAllowed:true
-          },
-          pointStart:100
-        }
+        text: "Y Ekseni"
+      }
+    },
+    xAxis: {
+      accessibility: {
+        rangeDescription: "2019 - 2024"
+      }
+    },
+    legend: {
+      layout: "vertical",
+      align: 'right',
+      verticalAlign: "middle"
+    },
+    plotOptions: {
+      series: {
+        label: {
+          connectorAllowed: true
+        },
+        pointStart: 100
       }
     }
+  };
 }
